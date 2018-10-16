a1f5faa6fe4a01453056ee994eb8e378fe6f4654
mwa <mwa@pfb03.cshl.edu>
2018-10-16 11:17:22 -0400

    Added a second script

--- Python_01/myFirstScript.py
+++ Python_01/myFirstScript.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 myName = 'Matthew Waas'
-
 print('My name: ',myName)
+
+myFaveColor = 'Purple'
+print('My favorite color: ', myFaveColor)
+
+myFaveActivity = 'Music'
+print('My favortie activity: ', myFaveActivity)
+
+myFaveAnimal = 'Elephant'
+print('My favortie animal: ', myFaveAnimal)
 
---
+++ Python_01/mySecondScript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+myName = sys.argv[1]
+myFaveColor = sys.argv[2]
+myFaveActivity = sys.argv[3]
+myFaveAnimal = sys.argv[4]
+
+print('My name:', myName,'\n','My favorite color:', myFaveColor, '\n', 'My favorite activity: ', myFaveActivity, '\n', 'My favortie animal: ', myFaveAnimal)
+
