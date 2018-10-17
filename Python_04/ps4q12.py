9dc065c5f78e59d5f3edf18c0420bc31c11301bf
mwa <mwa@pfb03.cshl.edu>
2018-10-17 19:00:27 -0400

    problem set 4 part 2

--- Python_04/ps4q12.py
+++ Python_04/ps4q12.py
@@ -7,4 +7,8 @@ q11len = [ (len(q11list[i]), q11list[i]) for i in range(4)]
 print(q11len)
   
 for i in range(4):
- print(q11len[i])
+ text=str(q11len[i])
+ text2=text.replace('(','')
+ text3=text2.replace(')','')
+ text4=text3.replace(', ','\t')
+ print(i+1,'\t',text4)
